9497198904c2cf60182ade1bb3baec3cc4c42b82
tadin24 <takumiinui24@gmail.com>
2022-07-24 01:32:44 +0700

    initial commit

---
+++ app/Database/Migrations/2022-07-23-182855_MsUser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class MsUser extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
---
+++ app/Database/Migrations/2022-07-23-182921_GroupUser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class GroupUser extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
